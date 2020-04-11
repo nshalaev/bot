64acb44453ce7cc70573505151390f7b9f435a31
nshalaev <shredermail@gmail.com>
2020-04-12 00:31:09 +0300

    add MongoDB document

---
+++ src/main/java/org/signal/bot/model/Subscriber.java
@@ -0,0 +1,14 @@
+package org.signal.bot.model;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Subscriber {
+    @Id
+    private ObjectId id;
+    private Long chatId;
+}
